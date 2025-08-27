6ed57f6533e346a60802aa6c7c78b1d483b2a717
fafmau <fa.fmau@gmail.com>
2025-08-27 17:58:02 +0200

    version 1.0

--- main.py
+++ main.py
@@ -272,7 +272,6 @@ def next_question():
 
 
 if __name__ == "__main__":
-    app.jinja_env.globals.update(enumerate=enumerate)
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
